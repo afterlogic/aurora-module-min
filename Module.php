ab7ecf3bb95a358fcc4bbe6ae71ba5abacb16ae4
nadine-shi <nadine@afterlogic.com>
2019-04-26 15:02:04 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -8,6 +8,8 @@
 namespace Aurora\Modules\Min;
 
 /**
+ * System module provides hash-based object storage.
+ * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
