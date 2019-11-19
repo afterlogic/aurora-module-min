3d2923469adb43dba5500a3c5fb6909e26c1694c
sash04ek <a.ovcharov@gmail.com>
2019-11-19 14:11:55 +0300

    fixed issue with ALTER TABLE statement

--- Storages/Db/sql/create.sql
+++ Storages/Db/sql/create.sql
@@ -8,5 +8,12 @@ CREATE TABLE IF NOT EXISTS `%PREFIX%min_hashes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-ALTER TABLE `%PREFIX%min_hashes` 
-ADD COLUMN `user_id` BIGINT(64) NULL DEFAULT NULL AFTER `hash_id`;
+SELECT count(*) INTO @exist
+  FROM information_schema.columns 
+    WHERE table_schema = database() AND COLUMN_NAME = 'user_id' AND table_name = 'au_min_hashes';
+
+SET @query = IF(@exist <= 0, 'ALTER TABLE `au_min_hashes` ADD COLUMN `user_id` BIGINT(64) NULL DEFAULT NULL AFTER `hash_id`', 'select 1 status');
+
+prepare stmt FROM @query;
+
+EXECUTE stmt;
