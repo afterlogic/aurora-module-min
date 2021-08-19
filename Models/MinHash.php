581e53e23422974628c566c8d5ec349732644a46
Alexandr Kuznetsov <akuz97@yandex.ru>
2021-08-19 18:02:39 +0300

    Created link to parents for models

--- Models/MinHash.php
+++ Models/MinHash.php
@@ -6,6 +6,11 @@ use Aurora\System\Classes\Model;
 class MinHash extends Model
 {
         protected $table = 'core_min_hashes';
+        
+        protected $primaryKey = 'HashId';
+	protected $foreignModel = 'Aurora\Modules\Core\Models\User';
+	protected $foreignModelIdColumn = 'UserId'; // Column that refers to an external table
+
         protected $fillable = [
                 'HashId',
                 'UserId',
